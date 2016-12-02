0ac0b9a04aa6707e53eae65fab56ff6918c1d7c6
Пустовойт Павел Александрович <pustovojt.pavel@gmail.com>
2016-12-03 00:52:20 +0300

    Read links from file to List

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -4,6 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 @Slf4j
 public class Application {
 
@@ -14,7 +22,26 @@ public class Application {
             return;
         }
 
+        try {
+            createTaskQueueFrom(
+                getLinkListFrom(settings.getFileName()));
+        } catch (NoSuchFileException e) {
+            log.error("File {} not found", settings.getFileName());
+        }
+        catch (IOException e) {
+            log.error("Error read file {}", settings.getFileName());
+        }
+    }
 
+    private static void createTaskQueueFrom(List<String> linkListFrom) {
+
+    }
+    private static List<String> getLinkListFrom(String fileName) throws IOException {
+        List<String> linkList = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            stream.forEach(linkList::add);
+        }
+        return linkList;
     }
 
     private static ApplicationSettings getApplicationSettings(String[] args) {
